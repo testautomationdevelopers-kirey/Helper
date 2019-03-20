6213ebfdcc2eeaf9cc4b9f433969878024f37354
matko <matko.smoljan@gmail.com>
2019-03-20 15:03:38 +0100

    Testing

--- src/test/java/elementHelpers/CheckDropdownExpanded.java
+++ src/test/java/elementHelpers/CheckDropdownExpanded.java
@@ -9,6 +9,7 @@ public class CheckDropdownExpanded {
 
 	public void waitForAttribute(WebElement waitThis){
 		WebDriverWait wait = new WebDriverWait(driver, 150); 
+		// aria-expanded daje informaciju o tome je li element expandan ili ne
 		// aria-expanded: true -> expanded; false -> not expanded
 		wait.until(ExpectedConditions.attributeToBe(ELEMENT, "aria-expanded", "true"));
 	}
